614d18ef10915a3a1ebe07aff79e1a5c38f4c76a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-29 02:11:25 +0530

    Added the scripts for adding the user dynamically...

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const searchBar = document.getElementById("filter");
 const userList = document.getElementById("result");
 
-const users = [];
+const listItems = [];
 getData();
 
 async function getData() {
   const res = await fetch("https://randomuser.me/api?results=50");
-  const data = await res.json();
-  console.log(data);
+  const { results } = await res.json();
+  userList.innerHTML = "";
+  console.log(results);
+
+  results.forEach((user) => {
+    let list = document.createElement("li");
+    list.innerHTML = `
+    <img
+      src="${user.picture.medium}"
+      alt="user-image"
+    />
+    <div class="user-info">
+      <h4>${user.name.title} ${user.name.first} ${user.name.last}</h4>
+      <p>${user.location.state}, ${user.location.country}</p>
+    </div>
+    `;
+    userList.appendChild(list);
+  });
 }
