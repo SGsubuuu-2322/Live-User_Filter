9909d465380dc10d0d3c5f5f78a8c8c87fbbcc35
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-29 02:51:46 +0530

    Added the scripts for filtering user...

--- script.js
+++ script.js
@@ -4,24 +4,37 @@ const userList = document.getElementById("result");
 const listItems = [];
 getData();
 
+searchBar.addEventListener("input", (e) => filterResults(e.target.value));
+
 async function getData() {
   const res = await fetch("https://randomuser.me/api?results=50");
   const { results } = await res.json();
   userList.innerHTML = "";
-  console.log(results);
+  //   console.log(results);
 
   results.forEach((user) => {
     let list = document.createElement("li");
+    listItems.push(list);
     list.innerHTML = `
     <img
       src="${user.picture.medium}"
-      alt="user-image"
+      alt="${user.name.first}"
     />
     <div class="user-info">
       <h4>${user.name.title} ${user.name.first} ${user.name.last}</h4>
-      <p>${user.location.state}, ${user.location.country}</p>
+      <p>${user.location.city}, ${user.location.country}</p>
     </div>
     `;
     userList.appendChild(list);
+  });
+}
+
+function filterResults(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
   });
 }
